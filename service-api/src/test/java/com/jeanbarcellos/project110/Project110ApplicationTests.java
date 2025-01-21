8f35847835ed903e20e8a5697a7f3e8bf798a2aa
Jean Barcellos <jeanbarcellos@hotmail.com>
2025-01-21 20:33:11 -0300

    First commit

---
+++ service-api/src/test/java/com/jeanbarcellos/project110/Project110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanbarcellos.project110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project110ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
