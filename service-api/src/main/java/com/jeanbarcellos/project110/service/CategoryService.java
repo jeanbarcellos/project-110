7086098e30a8fa6664e2278b64e1922a4a4ed953
Jean Barcellos <jeanbarcellos@hotmail.com>
2025-01-22 18:08:19 -0300

    Adição do cache e estratégias

--- service-api/src/main/java/com/jeanbarcellos/project110/service/CategoryService.java
+++ service-api/src/main/java/com/jeanbarcellos/project110/service/CategoryService.java
@@ -2,17 +2,24 @@ package com.jeanbarcellos.project110.service;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jeanbarcellos.project110.entity.Category;
 import com.jeanbarcellos.project110.repository.CategoryRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Pattern Write-Through
  *
  * Adicionar ao cache logo na criação
  */
+@Slf4j
 @Service
 public class CategoryService {
 
@@ -22,20 +29,54 @@ public class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    /**
+     * Recupera todas as categorias do banco de dados.
+     *
+     * - Usa cache para armazenar a lista completa de categorias com a chave 'all'.
+     * - Sempre consulta o cache antes de buscar no banco.
+     */
+    @Cacheable(value = "categories", key = "'all'")
     public List<Category> getAllCategories() {
+        doLongRunningTask();
+
         return this.categoryRepository.findAll();
     }
 
+    /**
+     * Recupera uma categoria específica pelo ID.
+     *
+     * - Usa cache para armazenar cada categoria individualmente com a chave baseada no ID.
+     * - O cache é preenchido na primeira chamada deste método para um ID específico.
+     */
+    @Cacheable(value = "categories", key = "#id")
     public Category getCategoryById(Long id) {
+        doLongRunningTask();
+
         return this.categoryRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Category not found: " + id));
     }
 
+    /**
+     * Cria uma nova categoria no banco de dados.
+     *
+     * - Adiciona ao cache o produto criado.
+     * - Invalida o cache da lista completa ('all').
+     */
+    @CachePut(value = "categories", key = "#result.id")
+    @CacheEvict(value = "categories", key = "'all'")
     @Transactional
     public Category createCategory(Category category) {
         return this.categoryRepository.save(category);
     }
 
+    /**
+     * Atualiza os dados de uma categoria existente.
+     *
+     * - Atualiza o cache da categoria específica.
+     * - Invalida o cache da lista completa ('all').
+     */
+    @CachePut(value = "categories", key = "#result.id")
+    @CacheEvict(value = "categories", key = "'all'")
     @Transactional
     public Category updateCategory(Long id, Category category) {
         Category entity = this.getCategoryById(id);
@@ -45,9 +86,27 @@ public class CategoryService {
         return this.categoryRepository.save(entity);
     }
 
+    /**
+     * Exclui uma categoria do banco de dados.
+     *
+     * - Remove o cache da categoria específica.
+     * - Invalida o cache da lista completa ('all').
+     */
+    @Caching(evict = {
+        @CacheEvict(value = "categories", key = "#id"),
+        @CacheEvict(value = "categories", key = "'all'") })
     @Transactional
     public void deleteCategory(Long id) {
         this.categoryRepository.deleteById(id);
     }
 
+    private void doLongRunningTask() {
+        log.info("Query no banco de dados");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- service-api/src/main/java/com/jeanbarcellos/project110/service/ProductService.java
+++ service-api/src/main/java/com/jeanbarcellos/project110/service/ProductService.java
@@ -2,16 +2,22 @@ package com.jeanbarcellos.project110.service;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import com.jeanbarcellos.project110.entity.Product;
 import com.jeanbarcellos.project110.repository.ProductRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Pattern Lazy Loading
  *
- * * Adicionar ao cache quando solicidao
+ * Adicionar ao cache quando solicidao
  */
+@Slf4j
 @Service
 public class ProductService {
 
@@ -21,17 +27,87 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    /**
+     * Recupera todos os produtos do banco de dados.
+     *
+     * - Usa cache para armazenar a lista completa de produtos com a chave 'all'.
+     * - O cache só é preenchido quando este método é chamado pela primeira vez.
+     * - Se o cache for inválido, os dados serão recarregados do banco.
+     */
+    @Cacheable(value = "products", key = "'all'")
     public List<Product> getAllProducts() {
+        doLongRunningTask();
+
         return this.productRepository.findAll();
     }
 
+    /**
+     * Recupera um produto específico pelo ID.
+     *
+     * - Usa cache para armazenar cada produto individualmente com a chave baseada no ID.
+     * - O cache só é preenchido na primeira chamada deste método para um ID específico.
+     */
+    @Cacheable(value = "products", key = "#id")
     public Product getProductById(Long id) {
+        doLongRunningTask();
+
         return this.productRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Product not found: " + id));
     }
 
+    /**
+     * Cria um novo produto no banco de dados.
+     *
+     * - Remove o cache da lista completa ('all') para garantir que ela seja recarregada na próxima consulta.
+     */
+    @CacheEvict(value = "products", key = "'all'")
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    /**
+     * Atualiza os dados de um produto existente.
+     *
+     * - Remove o cache do produto específico.
+     * - Remove o cache da lista completa ('all') para garantir que os dados estejam atualizados na próxima consulta.
+     */
+    @Caching(evict = {
+        @CacheEvict(value = "products", key = "#result.id"),
+        @CacheEvict(value = "products", key = "'all'") })
+    public Product updateProduct(Long id, Product product) {
+        Product entity = getProductById(id);
+
+        entity.setName(product.getName());
+        entity.setDescription(product.getDescription());
+        entity.setPrice(product.getPrice());
+
+        return productRepository.save(entity);
+    }
+
+    /**
+     * Exclui um produto do banco de dados.
+     *
+     * - Remove o cache do produto específico.
+     * - Remove o cache da lista completa ('all') para garantir que os dados estejam atualizados na próxima consulta.
+     */
+    @Caching(evict = {
+            @CacheEvict(value = "products", key = "#id"),
+            @CacheEvict(value = "products", key = "'all'") })
     public void deleteProduct(Long id) {
         this.productRepository.deleteById(id);
     }
 
+    @CacheEvict(value = "products", allEntries = true)
+    public void clearCache() {
+    }
+
+    private void doLongRunningTask() {
+        log.info("Query no banco de dados");
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
